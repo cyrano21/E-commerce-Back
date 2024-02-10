9cd8d490b2d3d80f0f3e035270ba239855a5a317
Louis Olivier <louiscyrano@gmail.com>
2024-02-10 18:10:31 +0100

    E-backend local

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const multer = require("multer");
-const path = require("path");
+
+//const fileUpload = require("express-fileupload");
 const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
 require("dotenv").config();
-const mongooseURL = process.env.MONGOOSE_URL;
+
+app.use(express.json());
+app.use(cors());
+
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000, // augmenter la limite pendant le développement
+});
+
+app.use(limiter);
+
+//app.use(fileUpload());
+
 const jwtSecret = process.env.JWT_SECRET;
 const backendUrl = process.env.BACKEND_URL || "http://localhost:4000";
 
-const Product = require("./models/Product");
-
-console.log(process.env.PORT, process.env.MONGOOSE_URL, process.env.JWT_SECRET);
-
-const corsOptions = {
-  origin: ["*", "https://main--mu-commerce-admin.netlify.app"],
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 // Database Connection With MongoDB
+const mongoose = require("mongoose");
+const mongooseURL = process.env.MONGOOSE_URL;
+
+const productSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  image: String,
+  category: String,
+  new_price: Number,
+  old_price: Number,
+});
+
+const Product = mongoose.model("Product", productSchema);
 
 mongoose
   .connect(mongooseURL, {
@@ -35,33 +56,90 @@ mongoose
     console.error("Error connecting to MongoDB", err);
   });
 
-//Image Storage Engine
-const storage = multer.diskStorage({
-  destination: "./upload/images",
-  filename: (req, file, cb) => {
-    console.log(file);
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
+// Configuration de Cloudinary avec vos informations d'authentification
+const cloud_name = process.env.CLOUD_NAME;
+const api_key = process.env.CLOUD_API_KEY;
+const api_secret = process.env.CLOUD_API_SECRET;
+
+cloudinary.config({
+  cloud_name,
+  api_key,
+  api_secret,
+  secure: true,
+  debug: true, // Active le logging détaillé
 });
 
-const upload = multer({ storage: storage });
-app.post("/upload", upload.single("product"), (req, res) => {
+// Ajoutez cette route à votre backend (app.js ou index.js)
+// Fonction de normalisation des catégories
+function normalizeCategory(category) {
+  const mapping = {
+    MEN: "Men",
+    WOMEN: "Women",
+    KID: "Kid",
+    // Ajoutez autant de mappages que nécessaire
+  };
+  return mapping[category.toUpperCase()] || category; // Retourne la catégorie normalisée ou la catégorie originale si non trouvée
+}
+
+// Exemple d'utilisation dans l'API /allproducts
+app.get("/allproducts", async (req, res) => {
+  try {
+    let products = await Product.find({});
+    // Normaliser la catégorie de chaque produit
+    products = products.map((product) => ({
+      ...product.toObject(),
+      category: normalizeCategory(product.category),
+    }));
+    res.json(products);
+  } catch (error) {
+    console.error("Error fetching all products:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+app.post("/upload", upload.single("picture"), async (req, res) => {
+  function formatCategory(category) {
+    if (!category) return "defaultCategory"; // Si la catégorie n'est pas fournie, utilisez une valeur par défaut
+    return category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
+  }
+
+  console.log("Category received:", req.body.category);
+  let category = req.body.category; // S'assurer que la catégorie est en majuscule
+
+  category = formatCategory(category);
+  const folder = `E-commerce/Category/${category}`;
+
   if (!req.file) {
     return res.status(400).json({
-      success: 0,
-      message: "No file uploaded",
+      success: false,
+      message: "Aucun fichier n'a été téléchargé.",
     });
   }
-  res.json({
-    success: 1,
-    image_url: `${backendUrl}/images/${req.file.filename}`, // Utilisation de backendUrl
-  });
+
+  const file = req.file;
+
+  try {
+    const result = await cloudinary.uploader.upload(file.path, {
+      folder: folder,
+    });
+    // Fonction pour formater la catégorie avec la première lettre en majuscule
+
+    res.json({
+      success: true,
+      message: "Fichier téléchargé avec succès",
+      image: result.secure_url,
+    });
+  } catch (error) {
+    console.error("Erreur lors du téléchargement sur Cloudinary:", error);
+    return res.status(500).json({
+      success: 0,
+      message: "Erreur lors du téléchargement du fichier.",
+      error: error.message,
+    });
+  }
 });
 
-app.use("/images", express.static("upload/images"));
+//app.use("/images", express.static("upload/images"));
 
 // MiddleWare to fetch user from database
 const fetchuser = async (req, res, next) => {
@@ -100,7 +178,7 @@ const Users = mongoose.model("Users", {
 });
 
 app.get("/", (req, res) => {
-  res.send("Root");
+  res.send("Welcome in your shop");
 });
 
 //Create an endpoint at ip/login for login the user and giving auth-token
@@ -137,8 +215,21 @@ app.post("/login", async (req, res) => {
   }
 });
 
+const Joi = require("joi");
 //Create an endpoint at ip/auth for regestring the user in data base & sending token
 app.post("/signup", async (req, res) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const { error, value } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
   console.log("Sign Up");
   let success = false;
   let check = await Users.findOne({ email: req.body.email });
@@ -170,10 +261,30 @@ app.post("/signup", async (req, res) => {
   res.json({ success, token });
 });
 
-app.get("/allproducts", async (req, res) => {
-  let products = await Product.find({});
-  console.log("All Products");
-  res.send(products);
+// Assurez-vous que multer est configuré pour gérer les téléchargements de fichiers.
+
+app.post("/addproduct", async (req, res) => {
+  try {
+    const { name, category, new_price, old_price, image } = req.body;
+
+    // Création d'une nouvelle instance du modèle Product sans fournir un `id`
+    const product = new Product({
+      name,
+      image, // Supposant que 'image' est bien l'URL retournée par Cloudinary
+      category,
+      new_price,
+      old_price,
+    });
+
+    await product.save();
+
+    console.log("product>>>>", product);
+
+    res.json({ success: true, product: product });
+  } catch (error) {
+    console.error("Error saving product:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/newcollections", async (req, res) => {
@@ -197,7 +308,7 @@ app.post("/addtocart", fetchuser, async (req, res) => {
     userData.cartData[req.body.itemId] += 1;
     await Users.findOneAndUpdate(
       { _id: req.user.id },
-      { cartData: userData.cartData }
+      { cartData: userData.cartData },
     );
     res.json({ message: "Added" }); // Modification ici
   } catch (error) {
@@ -210,12 +321,12 @@ app.post("/addtocart", fetchuser, async (req, res) => {
 app.post("/removefromcart", fetchuser, async (req, res) => {
   try {
     let userData = await Users.findOne({ _id: req.user.id });
-    if (userData.cartData[req.body.itemId] != 0) {
+    if (userData.cartData[req.body.itemId] !== 0) {
       userData.cartData[req.body.itemId] -= 1;
     }
     await Users.findOneAndUpdate(
       { _id: req.user.id },
-      { cartData: userData.cartData }
+      { cartData: userData.cartData },
     );
     res.json({ message: "Removed" });
   } catch (error) {
@@ -231,40 +342,30 @@ app.post("/getcart", fetchuser, async (req, res) => {
   res.json(userData.cartData);
 });
 
-app.post("/addproduct", async (req, res) => {
+app.post("/removeproduct", async (req, res) => {
   try {
-    let products = await Product.find({});
-    let id;
-    if (products.length > 0) {
-      let last_product_array = products.slice(-1);
-      let last_product = last_product_array[0];
-      id = last_product.id + 1;
-    } else {
-      id = 1;
+    const { _id } = req.body;
+
+    const product = await Product.findOneAndDelete({ _id: _id });
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
     }
-    const product = new Product({
-      id: id,
-      name: req.body.name,
-      image: req.body.image,
-      category: req.body.category,
-      new_price: req.body.new_price,
-      old_price: req.body.old_price,
-    });
-    await product.save();
-    console.log("Saved");
-    res.json({ success: true, name: req.body.name });
+    console.log("Removed:", product.name);
+    res.json({ success: true, name: product.name });
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error removing product:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 });
 
-app.post("/removeproduct", async (req, res) => {
-  const product = await Product.findOneAndDelete({ id: req.body.id });
-  console.log("Removed");
-  res.json({ success: true, name: req.body.name });
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, (error) => {
+  if (!error) console.log("Server Running on port " + port);
+  else console.log("Error : ", error);
 });

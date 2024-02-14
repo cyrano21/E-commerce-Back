1a16d47f388604bc2caa8fbc82b04bf55492331c
cyrano21 <louiscyrano@gmail.com>
2024-02-15 00:39:07 +0100

    modify User.js

--- models/User.js
+++ models/User.js
@@ -20,5 +20,5 @@ const usersSchema = new mongoose.Schema({
   },
 });
 
-const Users = mongoose.model("Users", usersSchema);
-module.exports = Users;
+const User = mongoose.model("User", usersSchema);
+module.exports = User;
